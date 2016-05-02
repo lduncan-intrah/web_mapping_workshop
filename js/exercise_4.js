a9602b5acedb544e57eb5b552dd3e75555cbd2b3
lduncan-intrah <lduncan@intrahealth.org>
2016-05-02 17:02:36 -0400

    Update exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -55,6 +55,10 @@ var clickHandler = function( e ) {
     
     $('#info').append(info);
   });
+  
+  var myGeoJSON = myLocation.getGeoJSON();
+  
+  getDirections( myGeoJSON.geometry.coordinates, feature.geometry.coordinates );
 }
 
 featureLayer.on('ready', function() {
@@ -82,3 +86,34 @@ map.on('locationfound', function(e) {
 });
 
 map.locate({setView: true});
+
+var routeLine = L.mapbox.featureLayer().addTo(map);
+
+function getDirections( from, to ) {
+  var jsonPayload = JSON.stringify({
+    locations: [
+        { lat: from[1], lon: from[0] },
+        { lat: to[1], lon: to[0] }
+      ],
+      costing: 'pedestrian',
+      units: 'miles'
+  });
+  $.ajax({
+    url: 'http://valhalla.mapzen.com/route',
+    data: { json: jsonPayload, api_key : 'valhalla-gwtf3x2' }
+  }).done( function( data ) {
+    var routeShape = polyline.decode( data.trip.legs[0].shape );
+    routeLine.setGeoJSON({
+      type: 'Feature',
+      geometry: {
+        type: 'LineString',
+        coordinates: routeShape
+      },
+      properties: {
+        "stroke": "#ed23f1",
+        "stroke-opacity": 0.8,
+        "stroke-width": 8
+      }
+    });
+  });
+}
